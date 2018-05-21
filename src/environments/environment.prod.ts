5ccdf1c557e0e14cc866d5a9bb85ae8e612562c3
Raphael <raphael.hauk@racing.tuwien.ac.at>
2018-05-21 18:49:13 +0200

    update mdc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  digitServiceUrl: "",
-  calendarServiceUrl: "",
-  identityUrl: "",
-  clientId: ""
+  digitServiceUrl: "https://digit-svc.azurewebsites.net",
+  calendarServiceUrl: "https://calendarsrv.azurewebsites.net",
+  identityUrl: "https://serviceidentity.azurewebsites.net",
+  clientId: "digitWebapp"
 };
