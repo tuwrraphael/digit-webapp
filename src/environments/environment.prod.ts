a185152d3d7e65326c3a16229dea9dd6cb207d3b
Raphael <raphael.hauk@racing.tuwien.ac.at>
2019-12-28 02:08:34 +0100

    add public config values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,8 @@ export const environment = {
   digitServiceUrl: "https://digit-svc.azurewebsites.net",
   calendarServiceUrl: "https://calendarsrv.azurewebsites.net",
   identityUrl: "https://serviceidentity.azurewebsites.net",
-  clientId: "digitWebapp"
+  travelServiceUrl:"https://travelsvc.azurewebsites.net",
+  clientId: "digitWebapp",
+  vapidPublicKey: "BAKuiCePCeMLk1W89DFkz9bFdV8zTKWzun0SB4WE8FM0TmAId5GeZC3MDWbeyGt5TrtGv6zBKFOmbo0qUNYm3iE",
+  pushServerUrl: "https://pushsvc.azurewebsites.net/"
 };
